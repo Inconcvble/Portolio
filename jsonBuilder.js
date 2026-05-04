ef9221b2e96b9a5b9d41b749d9cc25533cd68e92
Inconcvble <malulekentsakelo27@gmail.com>
2026-05-05 00:01:36 +0200

    Advancing to variable uploads and retrieving from html

--- jsonBuilder.js
+++ jsonBuilder.js
@@ -1,14 +1,52 @@
-const projects = ["Psychwave", "DND", "Ogre Bloodkour"];
+const tech = [];
+const screenshots = [];
+const videos = [];
 
+const projects = [];
 const projectList = { projects: [] };
 
-projects.forEach((project, index) => {
-  projectList.projects.push({
-    id: index + 1,
-    name: project,
-  });
-});
+function addTech() {
+  const value = document.getElementById("technologies").value;
+  tech.push(value);
+  document.getElementById("technologies").value = "";
+}
 
-const projectsJson = JSON.stringify(projectList, null, 2);
+function addScreenshots() {
+  const value = document.getElementById("screenshots").files[0];
+  screenshots.push(value);
+  document.getElementById("screenshots").value = "";
+}
 
-console.log(projectsJson);
+function addVideos() {
+  const value = document.getElementById("videos").files[0];
+  videos.push(value);
+  document.getElementById("videos").value = "";
+}
+
+function getProjData() {
+  const name = document.getElementById("projectName").value;
+  const status = document.getElementById("status").value;
+  const description = document.getElementById("description").value;
+  const liveDemo = document.getElementById("liveDemo").value;
+
+  loadProjects(name, status, description, liveDemo);
+}
+function loadProjects(name, status, description, liveDemo) {
+  const project = {
+    name: name,
+    status: status,
+    description: description,
+    technologies: tech,
+    screenshots: screenshots,
+    videos: videos,
+    liveDemo: liveDemo,
+  };
+  projects.push(project);
+}
+function createJson() {
+  const projectsJson = JSON.stringify(projects, null, 2);
+
+  console.log(projectsJson);
+}
+
+createJson();
