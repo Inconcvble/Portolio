44e8fdeee2d966623cacbc8b28f5ea0f044744c2
Inconcvble <malulekentsakelo27@gmail.com>
2026-05-04 22:11:30 +0200

    Basic json builder

---
+++ jsonBuilder.js
@@ -0,0 +1,12 @@
+const projects = ["Psychwave", "DND", "Ogre Bloodkour"];
+
+const projectList = { projects: [] };
+
+projects.forEach((project, index) => {
+  projectList.projects.push({
+    id: index + 1,
+    name: project,
+  });
+});
+
+const projectsJson = JSON.stringify(projectList, null, 2);
