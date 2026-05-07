168c01cc7eeb7316e789aff45b55a26873af0240
Inconcvble <malulekentsakelo27@gmail.com>
2026-05-07 20:03:47 +0200

    Refactor for server-client separation

--- save_load.js
+++
@@ -1,9 +0,0 @@
-import { writeFile } from "node:fs/promises";
-import { createJson } from "./data.js";
-
-const jsonString = createJson();
-
-writeFile("projects.json", jsonString, (err) => {
-  if (err) throw err;
-  console.log("File has been saved!");
-});
