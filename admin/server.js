5865ec78abe2da6735109d3bd3c011ce2797de05
Inconcvble <malulekentsakelo27@gmail.com>
2026-05-21 20:56:22 +0200

    Adjustment for render port

--- admin/server.js
+++ admin/server.js
@@ -122,4 +122,5 @@ app.put("/updateProject", async function (req, res) {
   }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
